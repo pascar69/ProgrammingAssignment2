4acebc400a04a2e740dac641c41281f4729cbca6
Pascar69 <pascar69@gmail.com>
2016-03-09 11:48:45 +0100

    Assignment done
    Assignment done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## N.B.: assuming that the matrix supplied is always invertible.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+    mm <<- m
+    im <<- solve(m)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has NOT changed) 
+## then the cache solve should retrieve the inverse from the cache.
+## N.B.: assuming that the matrix supplied is always invertible.
+
+cacheSolve <- function(m, ...) {
+    message(m)
+    message(mm)
+    message(!is.null(im))
+    message(identical(m, mm))
+    
+    if(!is.null(im) && identical(m, mm)) {
+        message("getting cached data")
+        return(im)
+    }
+    
+    message("calculate and cache data")
+    im <- makeCacheMatrix(m)
+    im
 }
+
+
+## examples of use
+m <- matrix(c(1:4), nrow=2)  ## creating a matrix 2x2
+im <- makeCacheMatrix (m)  
+
+cacheSolve(m)   ## getting cached data
+
+im <- NULL   ## erasing im if exist
+cacheSolve(m)   ## calculate the transpose (im not cached)
+
+m <- matrix(5:8, nrow=2)  ## changing the matrix
+cacheSolve(m)   ## calculate the transpose (m is changed!)
+
